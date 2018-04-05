e96d27d03a8e4848b74666be51993f543e4bb856
Dan Spitz <spitz.dan.l@gmail.com>
2018-04-05 07:22:50 -0400

    start at start

--- dist/bundle.js
+++ dist/bundle.js
@@ -1786,9 +1786,9 @@ let start = {};
 // start.experiences = ['woods, ending interpretation'];
 // start.experiences = ['bed, sitting up 2'];
 // start.experiences = ['woods, crossing the boundary 2'];
-start.experiences = ['woods, clearing'];
-start.has_regarded = { 'tangle, 3': true };
-start.has_understood = { 'tangle, 3': true };
+// start.experiences = ['woods, clearing'];
+// start.has_regarded = {'tangle, 3': true};
+// start.has_understood = {'tangle, 3': true};
 let world_driver = new commands_1.WorldDriver(new venience_world_1.VenienceWorld(start));
 ReactDom.render(React.createElement(Terminal_1.Terminal, { world_driver: world_driver }), document.getElementById('terminal'));
 
