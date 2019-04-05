4709d4ea3e8debd1add3a8be7f2c9aab8143e3f9
Dan Spitz <spitz.dan.l@gmail.com>
2019-04-05 13:48:24 -0400

    npm test works

--- src/tests/test_parser.ts
+++ src/tests/test_parser.ts
@@ -7,63 +7,64 @@ import {
 
 import { array_last } from '../typescript/datatypes';
 
-function test() {
-    function main_thread(p: Parser) {
-        p.consume([{token: 'look', token_type: { kind: 'Keyword' }}]);
+import 'mocha';
 
-        let who = p.split([
-            () => p.consume(['at', 'me'], 'me'),
-            () => p.consume(['at', 'mewtwo'], 'mewtwo'),
-            () => p.consume(['at', 'mewtwo', 'steve'], 'mewtwo steve'),
-            () => p.consume(['at', 'steven'], () => 'steven'),
-            () => p.consume(['at', 'martha'], 'martha'),
-            () => p.eliminate() 
-        ]);
+describe('parser', () => {
+    it('should do a thing', () => {
+        function main_thread(p: Parser) {
+            p.consume([{token: 'look', token_type: { kind: 'Keyword' }}]);
 
-        if (who === 'steven') {
-            p.eliminate();
+            let who = p.split([
+                () => p.consume(['at', 'me'], 'me'),
+                () => p.consume(['at', 'mewtwo'], 'mewtwo'),
+                () => p.consume(['at', 'mewtwo', 'steve'], 'mewtwo steve'),
+                () => p.consume(['at', 'steven'], () => 'steven'),
+                () => p.consume(['at', 'martha'], 'martha'),
+                () => p.eliminate() 
+            ]);
+
+            if (who === 'steven') {
+                p.eliminate();
+            }
+
+            let how = p.split([
+                () => p.consume([{ token: 'happily', typeahead_type: { kind: 'Locked' }}], 'happily'),
+                () => p.consume(['sadly'], 'sadly'),
+                () => 'neutrally'
+            ]);
+
+            p.submit();
+
+            return `Looked at ${who} ${how}`;
         }
 
-        let how = p.split([
-            () => p.consume([{ token: 'happily', typeahead_type: { kind: 'Locked' }}], 'happily'),
-            () => p.consume(['sadly'], 'sadly'),
-            () => 'neutrally'
-        ]);
+        let input: Token[] = ['look', 'at', 'steven'];
 
-        p.submit();
+        let [result, parses] = Parser.run_thread(main_thread, input);
 
-        return `Looked at ${who} ${how}`;
-    }
+        console.log(result);
 
-    let input: Token[] = ['look', 'at', 'steven'];
+        let id = input_display(parses, input);
+        console.log(id);
+        console.log(array_last(id.matches).type);
 
-    let [result, parses] = Parser.run_thread(main_thread, input);
-
-    console.log(result);
-
-    let id = input_display(parses, input);
-    console.log(id);
-    console.log(array_last(id.matches).type);
-
-    let ta = typeahead(parses, input);
-    console.log(ta);
-    if (ta.length > 0) {
-        console.log(array_last(ta[0]).type);
-    }
-    /*
-        TODO
-        Get rid of auto-option, it needs to be explicit
-        Change types of typeahead to support different typeahead styles
-            (new, old, locked)
-       
-        (Way Later) Write a tester that runs through every possible input for a given main_thread,
-        to find runtime error states
-            - ambiguous parses
-            - any other exceptions thrown
-    */
-
-}
-
-test()
+        let ta = typeahead(parses, input);
+        console.log(ta);
+        if (ta.length > 0) {
+            console.log(array_last(ta[0]).type);
+        }
+        /*
+            TODO
+            Get rid of auto-option, it needs to be explicit
+            Change types of typeahead to support different typeahead styles
+                (new, old, locked)
+           
+            (Way Later) Write a tester that runs through every possible input for a given main_thread,
+            to find runtime error states
+                - ambiguous parses
+                - any other exceptions thrown
+        */
+    });
+});
 
 
